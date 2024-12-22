0393e055faa3fc29c9b562647e497d62e2e76f7c
Code Forge Temple <code.forge.temple@gmail.com>
2024-12-22 02:41:31 +0200

    initial commit

---
+++ src/components/DrawBoard/DrawBoard.tsx
@@ -0,0 +1,124 @@
+/************************************************************************
+ *    Copyright (C) 2024 Code Forge Temple                              *
+ *    This file is part of circuit-sketcher-app project                 *
+ *    Licensed under the GNU General Public License v3.0.               *
+ *    See the LICENSE file in the project root for more information.    *
+ ************************************************************************/
+
+import React, {useEffect, useRef} from "react";
+import {LocalStorageManager, ModalAddImage} from "circuit-sketcher-core";
+
+import {CanvasManager} from "circuit-sketcher-core";
+import './DrawBoard.scss';
+import {saveAs} from "file-saver";
+import {loadJsonFile} from "../utils";
+import saveIcon from '../../assets/floppy-disk.svg';
+import loadIcon from '../../assets/download-circle.svg';
+
+const CANVAS_ID = "circuit-board";
+
+export const DrawBoard: React.FC = () => {
+    const canvasRef = useRef<HTMLDivElement>(null);
+    const resizeTimeoutRef = useRef<number | null>(null);
+    const canvasFileInputRef = useRef<HTMLInputElement>(null);
+    const libraryFileInputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (!canvasRef.current) return;
+
+        CanvasManager.setCanvasId(CANVAS_ID).getInstance().parse("[]");
+    }, []);
+
+    const handleSaveCanvas = async () => {
+        const jsonStr = await CanvasManager.getInstance().stringify();
+
+        const blob = new Blob([JSON.stringify(JSON.parse(jsonStr), null, 4)], {type: "application/json"});
+
+        saveAs(blob, "canvas.json");
+    };
+
+    const handleSaveLibrary = () => {
+        const blob = new Blob([LocalStorageManager.getLibrary(true)], {type: "application/json"});
+
+        saveAs(blob, "library.json");
+    };
+
+    const handleLoadCanvasClick = () => {
+        if (canvasFileInputRef.current) {
+            canvasFileInputRef.current.click();
+        }
+    };
+
+    const handleLoadLibraryClick = () => {
+        if (libraryFileInputRef.current) {
+            libraryFileInputRef.current.click();
+        }
+    };
+
+    const handleCanvasFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const jsonStr = await loadJsonFile(event);
+
+        CanvasManager.getInstance().parse(jsonStr);
+    };
+
+    const handleLibraryFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        LocalStorageManager.setLibrary(await loadJsonFile(event));
+    };
+
+    const handleResize = () => {
+        if (resizeTimeoutRef.current) {
+            clearTimeout(resizeTimeoutRef.current);
+        }
+
+        resizeTimeoutRef.current = window.setTimeout(async () => {
+            if (canvasRef.current) {
+                const jsonStr = await CanvasManager.getInstance().stringify();
+
+                CanvasManager.getInstance().parse(jsonStr);
+            }
+        }, 300);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+
+            if (resizeTimeoutRef.current) {
+                clearTimeout(resizeTimeoutRef.current);
+            }
+        };
+    }, []);
+
+    return (
+        <div className="canvas-container">
+            <ModalAddImage />
+            <div className="draw-board-menu">
+                <div className="btn-group canvas-action-bar" role="group" aria-label="Canvas">
+                    <button type="button" className="btn btn-secondary disabled" disabled>Canvas</button>
+                    <button type="button" className="btn btn-primary icon-btn" onClick={handleLoadCanvasClick}><img src={loadIcon} alt="Load..." /></button>
+                    <input
+                        type="file"
+                        ref={canvasFileInputRef}
+                        accept=".json"
+                        onChange={handleCanvasFileChange}
+                    />
+                    <button type="button" className="btn btn-primary icon-btn" onClick={handleSaveCanvas}><img src={saveIcon} alt="Save" /></button>
+                </div>
+                <div className="btn-group library-action-bar" role="group" aria-label="Library">
+                    <button type="button" className="btn btn-secondary disabled" disabled>Library</button>
+                    <button type="button" className="btn btn-primary icon-btn" onClick={handleLoadLibraryClick}><img src={loadIcon} alt="Load..." /></button>
+                    <input
+                        type="file"
+                        ref={libraryFileInputRef}
+                        accept=".json"
+                        onChange={handleLibraryFileChange}
+                    />
+                    <button type="button" className="btn btn-primary icon-btn" onClick={handleSaveLibrary} ><img src={saveIcon} alt="Save" /></button>
+                </div>
+            </div>
+            <div className="canvas" id={CANVAS_ID} ref={canvasRef}></div>
+        </div>
+    );
+};
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
+import path from 'path';
+
+
+export default defineConfig({
+    plugins: [react(), commonjs()],
+    resolve: {
+        dedupe: ["react", "react-dom"],
+        alias: {
+            '@modules': path.resolve(__dirname, 'node_modules'),
+        },
+    },
+    optimizeDeps: {
+        include: ['circuit-sketcher-core'],
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+            include: [/circuit-sketcher-core/, /node_modules/],
+        },
+    },
+});
